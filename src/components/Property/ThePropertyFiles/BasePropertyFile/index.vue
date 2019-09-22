446027f19b54f7f1ae9d0c9a322e89d0788f3f2d
vsalahkar <vincent.salahkar@gmail.com>
2019-09-22 19:03:03 +0200

    feat(property files): style and props

--- src/components/Property/ThePropertyFiles/BasePropertyFile/index.vue
+++ src/components/Property/ThePropertyFiles/BasePropertyFile/index.vue
@@ -1,11 +1,42 @@
 <template>
-    <div>
-
+    <div class="basePropertyFile">
+        <div class="basePropertyFile__icon"></div>
+        <div class="basePropertyFile__name">
+            <h1 class="basePropertyFile__fileName">{{fileName}}</h1>
+            <h2 class="basePropertyF__description">{{fileDescription}}</h2>
+        </div>
+        <div class="basePropertyFile__updates">
+            <div class="basePropertyFile__status">{{fileStatus}}</div>
+            <div class="basePropertyFile__time">{{fileUpdateTime}}</div>
+        </div>
     </div>
 </template>
 
 <script>
-export default {}
+export default {
+    props: {
+        fileName: {
+            type: String,
+            default: '',
+            required: true,
+        },
+        fileDescription: {
+            type: String,
+            default: '',
+            required: true,
+        },
+        fileStatus: {
+            type: String,
+            default: '',
+            required: true,
+        },
+        fileUpdateTime: {
+            type: String,
+            default: '',
+            required: true,
+        },
+    }
+}
 </script>
 
 <style scoped>
--- src/components/Property/ThePropertyFiles/index.vue
+++ src/components/Property/ThePropertyFiles/index.vue
@@ -3,20 +3,34 @@
         <header class="thePropertyFiles__header">
             <h1>247 Files</h1>
             <div class="thePropertyFiles__filterBar">
-                <input type="text">
-                <i></i>
+                <input type="text" placeholder="Search a file">
+                <i class="fa fa-search"></i>
             </div>
         </header>
         <section class="thePropertyFiles__tabs">
             <ul class="tabs">
-                <li class="tabs__tab"><a href="">Accountancy <span>42</span></a></li>
-                <li class="tabs__tab"><a href="">Accountancy <span>42</span></a></li>
-                <li class="tabs__tab"><a href="">Accountancy <span>42</span></a></li>
-                <li class="tabs__tab"><a href="">Accountancy <span>42</span></a></li>
+                <li class="tabs__tab">
+                    <button href="">Accountancy <span>42</span></button>
+                </li>
+                <li class="tabs__tab">
+                    <button href="">Accountancy <span>42</span></button>
+                </li>
+                <li class="tabs__tab">
+                    <button href="">Accountancy <span>42</span></button>
+                </li>
+                <li class="tabs__tab">
+                    <button href="">Accountancy <span>42</span></button>
+                </li>
             </ul>
         </section>
         <main class="theProperty__filesList">
-            <base-property-file></base-property-file>
+            <base-property-file
+                    :file-name="file.fileName"
+                    :file-description="file.fileDescription"
+                    :file-status="file.fileStatus"
+                    :file-update-time="file.fileUpdateTime"
+                    v-for="file in filesList"
+            />
         </main>
         <footer>
             <div class="footer__pages"></div>
@@ -40,10 +54,139 @@ import BasePropertyFile from './BasePropertyFile'
 export default {
     components: {
         BasePropertyFile
+    },
+    data: () => {
+        return {
+            filesList: [
+                {
+                    fileName: 'Ravalement de façade',
+                    fileDescription: 'Demande de devis envoyée à l\'architecte',
+                    fileStatus: 'Open',
+                    fileUpdateTime: '20/01/2019'
+                },
+                {
+                    fileName: 'Ravalement de façade',
+                    fileDescription: 'Demande de devis envoyée à l\'architecte',
+                    fileStatus: 'Open',
+                    fileUpdateTime: '20/01/2019'
+                },
+                {
+                    fileName: 'Ravalement de façade',
+                    fileDescription: 'Demande de devis envoyée à l\'architecte',
+                    fileStatus: 'Open',
+                    fileUpdateTime: '20/01/2019'
+                },
+                {
+                    fileName: 'Ravalement de façade',
+                    fileDescription: 'Demande de devis envoyée à l\'architecte',
+                    fileStatus: 'Open',
+                    fileUpdateTime: '20/01/2019'
+                },
+            ]
+        }
     }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+    @import "../../../styles/variables";
+    @import "../../../styles/base";
+
+    .thePropertyFiles {
+        background-color: $base-color-light-3;
+        padding: 32px 24px;
+
+        &__header {
+            padding: 8px 0;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+
+            h1 {
+                font-family: 'Rubik', sans-serif;
+                color: $base-color-dark;
+                font-size: 24px;
+                font-weight: bold;
+            }
+        }
+
+        &__filterBar {
+            position: relative;
+            margin-bottom: 12px;
+
+            input {
+                border-radius: 1000px;
+                background-color: $tertiary-color;
+                max-width: 350px;
+                flex: 1;
+                padding: 10px 48px 10px 24px;
+                color: $secondary-color;
+
+                &:focus {
+                    transition: background-color 0.1s ease-out;
+                    background-color: darken($tertiary-color, 5);
+                }
+            }
+
+            i {
+                position: absolute;
+                top: 50%;
+                right: 24px;
+                transform: translateY(-50%);
+                color: $base-color-light-2;
+            }
+        }
+
+        &__tabs {
+
+        }
+
+        .tabs {
+            display: flex;
+            padding: 16px 0;
+            border-bottom: 1px solid transparentize($base-color-light-2, 0.7);
+
+            &__tab {
+                font-size: 14px;
+                margin-right: 16px;
+
+                button {
+                    color: $base-color-light-2;
+                }
+
+                span {
+                    margin-left: 4px;
+                }
+            }
+        }
+
+        &__filesList {
+
+        }
+
+        .footer {
+            &__pages {
+
+            }
+
+            &__pagesNavigation {
+
+            }
+        }
+
+        .pagesNavigation {
+            &__previous {
+
+            }
+
+            &__next {
+
+            }
+
+            &__page {
+
+            }
+        }
+    }
 
 </style>
